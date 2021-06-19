19eb4de0466ed13bd6fbafd2779ff81b5448be83
ashutosh <kr.ashu465@gmail.com>
2021-06-19 09:27:42 +0530

    ✨ add project route

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,22 @@
-const projects = () => {
-  return <div className="p-4">projects page</div>;
+import { projects } from "../data";
+import ProjectCard from "../components/ProjectCard";
+
+const Projects = () => {
+  return (
+    <div className="p-4">
+      <nav className="">Navbar</nav>
+      <div className="relative grid grid-cols-12 gap-4 my-3">
+        {projects.map((project) => (
+          <div
+            key={projects.indexOf(project)}
+            className="col-span-12 p-2 rounded-md shadow-lg sm:col-span-6 lg:col-span-4 bg-gradient-to-tr from-glass-50 to-glass-70 dark:from-dark-200 dark:to-dark-200"
+          >
+            <ProjectCard data={project} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default projects;
+export default Projects;
--- types.ts
+++ types.ts
@@ -23,4 +23,4 @@ export interface IProject {
     key_techs: string[];
   }
   
-  export type Category = "react" | "next" | "vanilla" | "full-stack";
+  export type Category = "react" | "next";
