7ca220b0c794bc3dd69c4bd31bc6cfb3b006d187
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-06-23 05:59:58 +0530

    🐛 fix size/align issue in certain devices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,8 @@ function MyApp({ Component, pageProps, router }) {
       <ThemeProvider attribute="class">
         <div className="absolute rounded-full -z-10 top-30 right-10 w-44 h-44 bg-gradient-to-tr from-glass-70 to-glass-30"></div>
         <div className="absolute w-64 h-64 rounded-full -z-10 dark:invisible bg-gradient-to-tr from-glass-70 to-glass-30 bottom-20 left-20"></div>
+        <div className="flex items-center justify-center h-screen">
+
         <div className="z-20 grid grid-cols-12 gap-6 px-5 mt-4 text-sm lg:px-36 sm:px-20 md:px-32">
           <div className="col-span-12 p-4 text-center shadow-custom-light bg-gradient-to-tr from-glass-70 backdrop-filter dark:shadow-custom-dark backdrop-blur-sm to-glass-30 dark:from-dark-700 dark:to-dark-700 lg:col-span-3 rounded-2xl">
             <Sidebar />
@@ -24,6 +26,7 @@ function MyApp({ Component, pageProps, router }) {
               <Component {...pageProps} key={router.route} />
             </AnimatePresence>
           </div>
+        </div>
         </div>
       </ThemeProvider>
     </div>
